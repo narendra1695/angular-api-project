6d8c74a14c27ce3889cc5f56c4fccd34ebc6fe55
Narendra Singh <narendra@golden-technology.in>
2019-07-31 18:00:29 +0530

    Login component and wildcard routing
    Added login component: base component
    Added page-not-found-component: wildcard component
    Added routing for the same

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: LoginFormComponent
+  },
+  {
+    path: "**",
+    component: PageNotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
